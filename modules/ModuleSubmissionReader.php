5552b91d5f4158c13711f1063d99cc27ce4ada1b
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2016-07-29 14:09:47 +0200

    fixed module name in backend view

--- modules/ModuleSubmissionReader.php
+++ modules/ModuleSubmissionReader.php
@@ -14,7 +14,19 @@ use HeimrichHannot\FrontendEdit\ModuleReader;
 
 class ModuleSubmissionReader extends ModuleReader
 {
-	
-	
-	
+	public function generate()
+	{
+		if (TL_MODE == 'BE') {
+			$objTemplate           = new \BackendTemplate('be_wildcard');
+			$objTemplate->wildcard = '### ' . utf8_strtoupper($GLOBALS['TL_LANG']['FMD'][$this->type][0]) . ' ###';
+			$objTemplate->title    = $this->headline;
+			$objTemplate->id       = $this->id;
+			$objTemplate->link     = $this->name;
+			$objTemplate->href     = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
+			
+			return $objTemplate->parse();
+		}
+		
+		return parent::generate();
+	}
 }
